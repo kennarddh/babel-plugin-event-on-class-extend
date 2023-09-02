9aac6526046378a199864f73162b6b0fe8ecbb72
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-03 00:59:04 +0700

    feat: Example preset typescript

--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,24 @@
 import babel from '@babel/core'
 import babelPluginEventOnClassExtend from '../dist/index.js'
 
-const code = 'const n = 1'
+const code = `
+class X implements IOnExtendEvent {
+	static onExtend(targetClass: X) {
+		console.log(\`Class X is extended by \${targetClass.toString()}\`)
+	}
+}
+
+class Y extends X {
+	toString() {
+		return "Y"
+	}
+}
+`
 
 const output = babel.transformSync(code, {
+	filename: 'file.ts',
 	plugins: [babelPluginEventOnClassExtend],
+	presets: ['@babel/preset-typescript'],
 })
 
 console.log(output?.code)
