a964264290e96a24032a972039a8c521c00a0c37
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-05 17:20:07 +0700

    chore: Build cjs and mjs bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,13 +2,20 @@ import { RollupOptions } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 import typescriptModule from 'typescript'
 import del from 'rollup-plugin-delete'
+import packageJSON from './package.json' assert { type: 'json' }
 
 const RollupConfig: RollupOptions = {
 	input: 'src/index.ts',
-	output: {
-		file: 'dist/index.js',
-		format: 'esm',
-	},
+	output: [
+		{
+			file: packageJSON.exports['.'].import,
+			format: 'esm',
+		},
+		{
+			file: packageJSON.exports['.'].require,
+			format: 'cjs',
+		},
+	],
 	plugins: [
 		del({ targets: 'dist/*' }),
 		typescript({
